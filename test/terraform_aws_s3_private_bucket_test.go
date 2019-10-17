2fe53439bc1d1f20e7a2fa8322593a8c720689ad
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2019-10-16 17:34:31 -0700

    Add more terratests

--- test/terraform_aws_s3_private_bucket_test.go
+++ test/terraform_aws_s3_private_bucket_test.go
@@ -8,10 +8,37 @@ import (
 	awssdk "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/require"
 )
+
+func GetPublicAccessBlockConfiguration(t *testing.T, region string, bucketName string) *s3.PublicAccessBlockConfiguration {
+	config, err := GetPublicAccessBlockConfigurationE(t, region, bucketName)
+	require.NoError(t, err)
+	return config
+}
+
+func GetPublicAccessBlockConfigurationE(t *testing.T, region string, bucketName string) (*s3.PublicAccessBlockConfiguration, error) {
+	s3Client, err := aws.NewS3ClientE(t, region)
+
+	if err != nil {
+		return nil, err
+	}
+
+	params := &s3.GetPublicAccessBlockInput{
+		Bucket: awssdk.String(bucketName),
+	}
+
+	publicAccessBlock, err := s3Client.GetPublicAccessBlock(params)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return publicAccessBlock.PublicAccessBlockConfiguration, nil
+}
 
 func AssertS3BucketEncryptionEnabled(t *testing.T, region string, bucketName string) {
 	err := AssertS3BucketEncryptionEnabledE(t, region, bucketName)
@@ -46,8 +73,92 @@ func AssertS3BucketEncryptionEnabledE(t *testing.T, region string, bucketName st
 	return nil
 }
 
-// An example of how to test the Terraform module in examples/terraform-aws-s3-example using Terratest.
-func TestTerraformAwsS3Example(t *testing.T) {
+func AssertS3BucketBlockPublicACLEnabled(t *testing.T, region string, bucketName string) {
+	err := AssertS3BucketPublicAccessBlockConfigurationEnabledE(t, region, bucketName, "BlockPublicAcls")
+	require.NoError(t, err)
+}
+
+func AssertS3BucketBlockPublicPolicyEnabled(t *testing.T, region string, bucketName string) {
+	err := AssertS3BucketPublicAccessBlockConfigurationEnabledE(t, region, bucketName, "BlockPublicPolicy")
+	require.NoError(t, err)
+}
+
+func AssertS3BucketIgnorePublicACLEnabled(t *testing.T, region string, bucketName string) {
+	err := AssertS3BucketPublicAccessBlockConfigurationEnabledE(t, region, bucketName, "IgnorePublicAcls")
+	require.NoError(t, err)
+}
+
+func AssertS3BucketRestrictPublicBucketsEnabled(t *testing.T, region string, bucketName string) {
+	err := AssertS3BucketPublicAccessBlockConfigurationEnabledE(t, region, bucketName, "RestrictPublicBuckets")
+	require.NoError(t, err)
+}
+
+func AssertS3BucketPublicAccessBlockConfigurationEnabledE(t *testing.T, region string, bucketName string, configType string) error {
+	config := GetPublicAccessBlockConfiguration(t, region, bucketName)
+
+	expected := true
+	switch configType {
+	case "BlockPublicAcls":
+		if *config.BlockPublicAcls != expected {
+			return fmt.Errorf("Block public ACLs not enabled")
+		}
+	case "BlockPublicPolicy":
+		if *config.BlockPublicPolicy != expected {
+			return fmt.Errorf("Block public policy not enabled")
+		}
+	case "IgnorePublicAcls":
+		if *config.IgnorePublicAcls != expected {
+			return fmt.Errorf("Ignore public ACLs not enabled")
+		}
+	case "RestrictPublicBuckets":
+		if *config.RestrictPublicBuckets != expected {
+			return fmt.Errorf("Restrict public buckets not enabled")
+		}
+	default:
+		return fmt.Errorf("Unrecognized public access block configuration type")
+	}
+
+	return nil
+}
+
+func AssertS3BucketLoggingEnabled(t *testing.T, region string, bucketName string, loggingBucketName string) {
+	err := AssertS3BucketLoggingEnabledE(t, region, bucketName, loggingBucketName)
+	require.NoError(t, err)
+}
+
+func AssertS3BucketLoggingEnabledE(t *testing.T, region string, bucketName string, loggingBucketName string) error {
+	s3Client, err := aws.NewS3ClientE(t, region)
+
+	if err != nil {
+		return err
+	}
+
+	params := &s3.GetBucketLoggingInput{
+		Bucket: awssdk.String(bucketName),
+	}
+
+	bucketLogging, err := s3Client.GetBucketLogging(params)
+
+	if err != nil {
+		return err
+	}
+
+	loggingEnabled := bucketLogging.LoggingEnabled
+
+	if loggingEnabled == nil {
+		return fmt.Errorf("Logging not enabled")
+	}
+
+	actual := *loggingEnabled.TargetBucket
+	expected := loggingBucketName
+	if actual != expected {
+		return fmt.Errorf("Logging target bucket does not match expected. Got: %v, Expected: %v", actual, expected)
+	}
+
+	return nil
+}
+
+func TestTerraformAwsS3PrivateBucket(t *testing.T) {
 	t.Parallel()
 
 	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
@@ -81,7 +192,7 @@ func TestTerraformAwsS3Example(t *testing.T) {
 	// Create temporary logging bucket for s3 bucket module
 	s3Client, err := aws.NewS3ClientE(t, awsRegion)
 	if err != nil {
-		fmt.Println(err)
+		logger.Log(t, err)
 		return
 	}
 
@@ -92,7 +203,7 @@ func TestTerraformAwsS3Example(t *testing.T) {
 
 	_, err = s3Client.CreateBucket(params)
 	if err != nil {
-		fmt.Println(err)
+		logger.Log(t, err)
 		return
 	}
 
@@ -106,4 +217,10 @@ func TestTerraformAwsS3Example(t *testing.T) {
 	bucketID := terraform.Output(t, terraformOptions, "id")
 
 	AssertS3BucketEncryptionEnabled(t, awsRegion, bucketID)
+	aws.AssertS3BucketVersioningExists(t, awsRegion, bucketID)
+	AssertS3BucketBlockPublicACLEnabled(t, awsRegion, bucketID)
+	AssertS3BucketBlockPublicPolicyEnabled(t, awsRegion, bucketID)
+	AssertS3BucketIgnorePublicACLEnabled(t, awsRegion, bucketID)
+	AssertS3BucketRestrictPublicBucketsEnabled(t, awsRegion, bucketID)
+	AssertS3BucketLoggingEnabled(t, awsRegion, bucketID, expectedLoggingBucket)
 }
