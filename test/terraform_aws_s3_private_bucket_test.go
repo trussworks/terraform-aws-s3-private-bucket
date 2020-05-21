233f5b16a2f6f62f3c54878b1e4b6422a8cd9b1f
isty <misty@truss.works>
2020-05-21 14:52:18 -0400

    add test for absence of analytics using preexisting example bucket

--- test/terraform_aws_s3_private_bucket_test.go
+++ test/terraform_aws_s3_private_bucket_test.go
@@ -373,3 +373,77 @@ func TestTerraformAwsS3PrivateBucketNoLoggingBucket(t *testing.T) {
 	AssertS3BucketLoggingNotEnabled(t, awsRegion, testName)
 	AssertS3BucketPolicyContainsNonTLSDeny(t, awsRegion, testName)
 }
+
+func AssertS3BucketAnalyticsNotEnabled(t *testing.T, region string, bucketName string) {
+	err := AssertS3BucketAnalyticsNotEnabledE(t, region, bucketName)
+	require.NoError(t, err)
+}
+
+func AssertS3BucketAnalyticsNotEnabledE(t *testing.T, region string, bucketName string) error {
+	s3client, err := aws.NewS3ClientE(t, region)
+
+	if err != nil {
+		return err
+	}
+
+	params := &s3.GetBucketAnalyticsConfigurationInput{
+		Bucket: awssdk.String(bucketName),
+	}
+
+	bucketAnalytics, err := s3client.GetBucketAnalyticsConfiguration(params)
+
+	if err != nil {
+		return err
+	}
+
+	analyticsEnabled := bucketAnalytics.AnalyticsConfiguration
+
+	if analyticsEnabled == nil {
+		return fmt.Errorf("Analytics is not enabled")
+	}
+
+	return nil
+}
+
+func TestTerraformAwsS3PrivateBucketNoLoggingNoAnalytics(t *testing.T) {
+	t.Parallel()
+
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, "../", "examples/simple-no-logging")
+
+	// Give this S3 Bucket a unique ID for a name tag so we can distinguish it from any other Buckets provisioned
+	// in your AWS account
+	testName := fmt.Sprintf("terratest-aws-s3-private-bucket-no-logging-no-analytics-%s", strings.ToLower(random.UniqueId()))
+	awsRegion := "us-west-2"
+
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: tempTestFolder,
+
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"test_name": testName,
+			"region":    awsRegion,
+		},
+
+		// Environment variables to set when running Terraform
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	aws.AssertS3BucketExists(t, awsRegion, testName)
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	AssertS3BucketAnalyticsNotEnabled(t, awsRegion, testName)
+	AssertS3BucketLoggingNotEnabled(t, awsRegion, testName)
+	AssertS3BucketPolicyContainsNonTLSDeny(t, awsRegion, testName)
+}
