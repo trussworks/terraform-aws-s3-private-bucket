cafcc6da666a93e45729f85559b0dc8ad9b75b27
Chikara Takahashi <chtakahashi10@gmail.com>
2023-07-17 11:16:46 -0400

    remove deprecated attributes from s3 bucket ignore_changes block

--- main.tf
+++ main.tf
@@ -95,19 +95,11 @@ resource "aws_s3_bucket" "private_bucket" {
     # path from version 3.x of the AWS provider to version 4.x
     ignore_changes = [
       # While no special usage instructions are documented for needing this
-      # ignore_changes rule, changes are still detected during the upgrade
-      # process, so this serves to avoid drift detection since the
-      # aws_s3_bucket_policy will be used instead.
-      policy,
-
-      # While no special usage instructions are documented for needing this
       # ignore_changes rule, this should avoid drift detection if conflicts
       # with the aws_s3_bucket_versioning exist.
       versioning,
 
       # https://registry.terraform.io/providers/hashicorp%20%20/aws/3.75.2/docs/resources/s3_bucket_acl#usage-notes
-      acceleration_status,
-      acl,
       grant,
 
       # https://registry.terraform.io/providers/hashicorp%20%20/aws/3.75.2/docs/resources/s3_bucket_cors_configuration#usage-notes
