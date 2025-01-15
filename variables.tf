fe7c79dc1b59e70a51b05055dbe396d3b9f59a03
Barry Morrison <bdmorrison@truss.works>
2025-01-14 18:52:11 -0600

    fix: sets proper type on variable

--- variables.tf
+++ variables.tf
@@ -1,66 +1,30 @@
+variable "abort_incomplete_multipart_upload_days" {
+  description = "Number of days until aborting incomplete multipart uploads"
+  type        = number
+  default     = 14
+}
+
+variable "additional_lifecycle_rules" {
+  description = "List of additional lifecycle rules to specify"
+  type        = list(any)
+  default     = []
+}
+
 variable "bucket" {
   description = "The name of the bucket."
   type        = string
 }
 
-variable "use_random_suffix" {
-  description = "Whether to add a random suffix to the bucket name."
+variable "bucket_key_enabled" {
+  description = "Whether or not to use Amazon S3 Bucket Keys for SSE-KMS."
   type        = bool
   default     = false
 }
 
-variable "use_account_alias_prefix" {
-  description = "Whether to prefix the bucket name with the AWS account alias."
-  type        = string
+variable "control_object_ownership" {
+  description = "Whether to manage S3 Bucket Ownership Controls on this bucket."
+  type        = bool
   default     = true
-}
-
-variable "custom_bucket_policy" {
-  description = "JSON formatted bucket policy to attach to the bucket."
-  type        = string
-  default     = ""
-}
-
-variable "logging_bucket" {
-  description = "The S3 bucket to send S3 access logs."
-  type        = string
-  default     = ""
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to the bucket."
-  default     = {}
-  type        = map(string)
-}
-
-variable "enable_bucket_inventory" {
-  type        = bool
-  default     = false
-  description = "If set to true, Bucket Inventory will be enabled."
-}
-
-variable "enable_bucket_force_destroy" {
-  type        = bool
-  default     = false
-  description = "If set to true, Bucket will be emptied and destroyed when terraform destroy is run."
-}
-
-variable "inventory_bucket_format" {
-  type        = string
-  default     = "ORC"
-  description = "The format for the inventory file. Default is ORC. Options are ORC or CSV."
-}
-
-variable "schedule_frequency" {
-  type        = string
-  default     = "Weekly"
-  description = "The S3 bucket inventory frequency. Defaults to Weekly. Options are 'Weekly' or 'Daily'."
-}
-
-variable "enable_analytics" {
-  description = "Enables storage class analytics on the bucket."
-  default     = true
-  type        = bool
 }
 
 variable "cors_rules" {
@@ -69,20 +33,34 @@ variable "cors_rules" {
   default     = []
 }
 
-variable "versioning_status" {
-  description = "A string that indicates the versioning status for the log bucket."
-  default     = "Enabled"
+variable "custom_bucket_policy" {
+  description = "JSON formatted bucket policy to attach to the bucket."
   type        = string
-  validation {
-    condition     = contains(["Enabled", "Disabled", "Suspended"], var.versioning_status)
-    error_message = "Valid values for versioning_status are Enabled, Disabled, or Suspended."
-  }
+  default     = ""
 }
 
-variable "abort_incomplete_multipart_upload_days" {
-  description = "Number of days until aborting incomplete multipart uploads"
-  type        = number
-  default     = 14
+variable "enable_analytics" {
+  description = "Enables storage class analytics on the bucket."
+  default     = true
+  type        = bool
+}
+
+variable "enable_bucket_force_destroy" {
+  type        = bool
+  default     = false
+  description = "If set to true, Bucket will be emptied and destroyed when terraform destroy is run."
+}
+
+variable "enable_bucket_inventory" {
+  type        = bool
+  default     = false
+  description = "If set to true, Bucket Inventory will be enabled."
+}
+
+variable "enable_s3_public_access_block" {
+  description = "Bool for toggling whether the s3 public access block resource should be enabled."
+  type        = bool
+  default     = true
 }
 
 variable "expiration" {
@@ -95,10 +73,28 @@ variable "expiration" {
   ]
 }
 
-variable "transitions" {
-  description = "Current version transition blocks"
-  type        = list(any)
-  default     = []
+variable "inventory_bucket_format" {
+  type        = string
+  default     = "ORC"
+  description = "The format for the inventory file. Default is ORC. Options are ORC or CSV."
+}
+
+variable "kms_master_key_id" {
+  description = "The AWS KMS master key ID used for the SSE-KMS encryption. If blank, bucket encryption configuration defaults to AES256."
+  type        = string
+  default     = ""
+}
+
+variable "logging_bucket" {
+  description = "The S3 bucket to send S3 access logs."
+  type        = string
+  default     = ""
+}
+
+variable "noncurrent_version_expiration" {
+  description = "Number of days until non-current version of object expires"
+  type        = number
+  default     = 365
 }
 
 variable "noncurrent_version_transitions" {
@@ -112,48 +108,6 @@ variable "noncurrent_version_transitions" {
   ]
 }
 
-variable "noncurrent_version_expiration" {
-  description = "Number of days until non-current version of object expires"
-  type        = number
-  default     = 365
-}
-
-variable "kms_master_key_id" {
-  description = "The AWS KMS master key ID used for the SSE-KMS encryption. If blank, bucket encryption configuration defaults to AES256."
-  type        = string
-  default     = ""
-}
-
-variable "enable_s3_public_access_block" {
-  description = "Bool for toggling whether the s3 public access block resource should be enabled."
-  type        = bool
-  default     = true
-}
-
-variable "bucket_key_enabled" {
-  description = "Whether or not to use Amazon S3 Bucket Keys for SSE-KMS."
-  type        = bool
-  default     = false
-}
-
-variable "transfer_acceleration" {
-  description = "Whether or not to enable bucket acceleration."
-  type        = bool
-  default     = null
-}
-
-variable "additional_lifecycle_rules" {
-  description = "List of additional lifecycle rules to specify"
-  type        = list(any)
-  default     = []
-}
-
-variable "control_object_ownership" {
-  description = "Whether to manage S3 Bucket Ownership Controls on this bucket."
-  type        = bool
-  default     = true
-}
-
 variable "object_ownership" {
   description = "Object ownership. Valid values: BucketOwnerEnforced, BucketOwnerPreferred or ObjectWriter."
   type        = string
@@ -164,4 +118,50 @@ variable "s3_bucket_acl" {
   description = "Set bucket ACL per [AWS S3 Canned ACL](<https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl>) list."
   default     = null
   type        = string
+}
+
+variable "schedule_frequency" {
+  type        = string
+  default     = "Weekly"
+  description = "The S3 bucket inventory frequency. Defaults to Weekly. Options are 'Weekly' or 'Daily'."
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the bucket."
+  default     = {}
+  type        = map(string)
+}
+
+variable "transfer_acceleration" {
+  description = "Whether or not to enable bucket acceleration."
+  type        = bool
+  default     = null
+}
+
+variable "transitions" {
+  description = "Current version transition blocks"
+  type        = list(any)
+  default     = []
+}
+
+variable "use_account_alias_prefix" {
+  description = "Whether to prefix the bucket name with the AWS account alias."
+  type        = bool
+  default     = true
+}
+
+variable "use_random_suffix" {
+  description = "Whether to add a random suffix to the bucket name."
+  type        = bool
+  default     = false
+}
+
+variable "versioning_status" {
+  description = "A string that indicates the versioning status for the log bucket."
+  default     = "Enabled"
+  type        = string
+  validation {
+    condition     = contains(["Enabled", "Disabled", "Suspended"], var.versioning_status)
+    error_message = "Valid values for versioning_status are Enabled, Disabled, or Suspended."
+  }
 }
